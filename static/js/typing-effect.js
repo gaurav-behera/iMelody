f08573d055c827d4ae6522391b922079a0c2e655
Gaurav <gaurav.behera@research.iiit.ac.in>
2023-05-05 19:01:57 +0530

    Modified directory structure and navbar

---
+++ static/js/typing-effect.js
@@ -0,0 +1,16 @@
+var i = 0;
+var text = "Start typing to find your next melody!";
+
+var speed = 40;
+
+function typeWriter() {
+	if (i < text.length) {
+		document.getElementById("typing-text").textContent += text.charAt(i);
+		i++;
+	}
+	setTimeout(typeWriter, speed);
+}
+
+window.onload = function () {
+	typeWriter();
+};
